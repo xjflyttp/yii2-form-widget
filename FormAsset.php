e15248b5c32a6072b1b432c76f82bb21b4fe1893
xjflyttp <xjflyttp@gmail.com>
2015-03-06 16:08:23 +0800

    update

--- FormAsset.php
+++ FormAsset.php
@@ -6,25 +6,10 @@ use yii\web\AssetBundle;
 
 class FormAsset extends AssetBundle {
 
-    public $sourcePath;
+    public $sourcePath = '@vendor/xj/yii2-form-widget/assets';
     public $basePath = '@webroot/assets';
-    public $publishOptions = ['forceCopy' => YII_DEBUG];
     public $css = [];
-    public $js = [];
+    public $js = ['jquery.form.js'];
     public $depends = ['yii\web\JqueryAsset'];
-
-    private function getJs() {
-        return [
-            YII_DEBUG ? 'jquery.form.js' : 'jquery.form.min.js',
-        ];
-    }
-
-    public function init() {
-        $this->sourcePath = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'assets';
-        if (empty($this->js)) {
-            $this->js = $this->getJs();
-        }
-        return parent::init();
-    }
 
 }
