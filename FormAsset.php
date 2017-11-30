4a6bf058a18d67b559b7c2a80312f6605d889796
xjflyttp <xjflyttp@gmail.com>
2017-11-30 10:53:21 +0800

    fix basePath

--- FormAsset.php
+++ FormAsset.php
@@ -7,7 +7,6 @@ use yii\web\AssetBundle;
 class FormAsset extends AssetBundle {
 
     public $sourcePath = '@vendor/xj/yii2-form-widget/assets';
-    public $basePath = '@webroot/assets';
     public $css = [];
     public $js = ['jquery.form.js'];
     public $depends = ['yii\web\JqueryAsset'];
